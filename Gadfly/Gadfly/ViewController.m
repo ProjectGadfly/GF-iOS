ab06ec7faa63e1602da9c5a415c04e7d6905f8ac
M Hardt <hardtmad@grinnell.edu>
2017-03-24 11:54:05 -0500

    Add modal segue to legislators page. No cells are displayed

--- Gadfly/Gadfly/ViewController.m
+++ Gadfly/Gadfly/ViewController.m
@@ -1,4 +1,5 @@
 #import "ViewController.h"
+#import "LegislatorViewController.h"
 
 @interface ViewController ()
 
@@ -15,6 +16,18 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([segue.identifier isEqualToString:@"showLegislators"]) {
+        //NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
+        //LegislatorViewController *destViewController = segue.destinationViewController;
+        //destViewController.recipeName = [recipes objectAtIndex:indexPath.row];
+    }
+}
+
+- (IBAction)submitPressed:(id)sender {
+    [self performSegueWithIdentifier:@"showLegislators" sender:self];
 }
 
 
