0024448037dd190b83383656beffa1a629edf5d2
M Hardt <hardtmad@grinnell.edu>
2017-03-28 15:24:42 -0500

    Edit for camera permissions

---
+++ PlayingWithZXing/PlayingWithZXing/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PlayingWithZXing
+//
+//  Created by Madeleine Hardt on 3/28/17.
+//  Copyright © 2017 Madeleine Hardt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
