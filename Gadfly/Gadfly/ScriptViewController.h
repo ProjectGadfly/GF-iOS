93f2d05a0ba6e233f4e74e975cbae3442df44c69
M Hardt <hardtmad@grinnell.edu>
2017-03-29 17:38:15 -0500

    Add call to server to get legislator data for script page. This should be refactored at some point. Need to refresh table view in completion handler. Harder because this view does nto inherit from UI table view

--- Gadfly/Gadfly/ScriptViewController.h
+++ Gadfly/Gadfly/ScriptViewController.h
@@ -1,5 +1,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ScriptViewController : UIViewController
-
+@property (weak, nonatomic) IBOutlet UILabel *scriptTitle;
+@property (weak, nonatomic) IBOutlet UITextView *callScript;
+@property (nonatomic, strong) NSMutableArray *legislators;
 @end
